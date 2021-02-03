81d72ff57ae93834930f7c792bc5ee596d2a4af9
Erik Zhao <erikzhao98@gmail.com>
2021-02-03 16:14:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Write your code here!
 document.querySelector("main#main").remove();
 
-let newHeader = document.querySelector("body").innerHTML = '<h1 id="victory"></h1>';
-newHeader.innerHTML = "Erik is the champion";
-
-// let newHeader = document.querySelector("body").innerHTML = '<h1 id="victory">Erik is the champion</h1>';
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Erik is the champion"
 
 
