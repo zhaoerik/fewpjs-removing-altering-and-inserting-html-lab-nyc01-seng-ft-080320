dc3a4d772c524af5fcfc47f98df3517a21a4dc34
Erik Zhao <erikzhao98@gmail.com>
2021-02-03 16:14:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Erik is the champion"
+
+
